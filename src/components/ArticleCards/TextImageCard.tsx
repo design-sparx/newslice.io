3b84a2ab6244dd54864fac6c46c1a7a9f9ffff10
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2022-09-03 16:27:11 +0300

    - UI updates

--- src/components/ArticleCards/TextImageCard.tsx
+++ src/components/ArticleCards/TextImageCard.tsx
@@ -1,4 +1,14 @@
-import { Avatar, Card, Center, createStyles, Group, Image, MantineTheme, Stack, Text } from '@mantine/core';
+import {
+  Avatar,
+  Card,
+  Center,
+  createStyles,
+  Group,
+  Image,
+  MantineTheme,
+  Stack,
+  Text,
+} from '@mantine/core';
 import { Article } from '../../constants/articles';
 import { Size } from '../../constants/cardSizes';
 
@@ -121,9 +131,9 @@ const TextImageCard = ({ className, article, size, showDescription }: ArticleCar
                     {name}
                   </Text>
 
-                  <Text className={classes.description} size='sm' lineClamp={3}>
-                    {description}
-                  </Text>
+                    <Text className={classes.description} size='sm' lineClamp={3}>
+                      {description}
+                    </Text>
 
                   <Group noWrap spacing={4} className={classes.footer}>
                     <Center>
